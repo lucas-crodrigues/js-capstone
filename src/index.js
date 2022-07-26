4b08ae597ae32b0cb692f6fa241255e4bad304b3
lucascr <lcrodrigues1995@gmail.com>
2022-07-26 20:00:59 -0300

    add event listeners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './styles.css';
+import getCardInfo from './modules/cardsInfo.js';
+import renderCards from './modules/populateHTML.js';
+
+window.addEventListener('DOMContentLoaded', () => getCardInfo());
+
+document.querySelector('.logo').addEventListener('click', () => {
+  document.querySelector('.cards').innerHTML = '';
+  renderCards();
+});
