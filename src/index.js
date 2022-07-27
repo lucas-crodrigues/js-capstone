a9b051a0666e06edc743d3d8002d52295d4a2294
lucascr <lcrodrigues1995@gmail.com>
2022-07-27 18:10:20 -0300

    Check if likes are being received

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import './styles.css';
 import getCardInfo from './modules/cardsInfo.js';
 import renderCards from './modules/populateHTML.js';
+import { getLike } from './modules/invLikes.js'
 
 window.addEventListener('DOMContentLoaded', async () => {
   getCardInfo(renderCards);
+});
+
+window.addEventListener('DOMContentLoaded', async () => {
+  console.log(getLike());
 });
