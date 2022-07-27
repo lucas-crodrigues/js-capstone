bcddbd24904c029711463bd64a3bd8703a2dca3d
lucascr <lcrodrigues1995@gmail.com>
2022-07-27 13:36:48 -0300

    Update cardsInfo.js

--- src/modules/cardsInfo.js
+++ src/modules/cardsInfo.js
@@ -1,17 +1,17 @@
+import getCardNames from '../modules/sessionCards.js';
 
-
-const getCardInfo = async () => {
+const getCardInfo = async (renderCards) => {
   let cardInfo = [];
+  await getCardNames();
   const sessionCards = JSON.parse(localStorage.getItem('sessionCards'));
   sessionCards.forEach(async (card) => {
-    fetch(`https://api.scryfall.com/cards/named?fuzzy=${card}`).then(async (response) => {
+    await fetch(`https://api.scryfall.com/cards/named?fuzzy=${card}`).then(async (response) => {
       const dataArr = await response.json();
       cardInfo.push(dataArr);
       localStorage.setItem('sessionCardsInfo', JSON.stringify(cardInfo));
     });
   })
-  console.log(cardInfo);
-  return cardInfo;
+  renderCards();
 }
 
 export default getCardInfo;
