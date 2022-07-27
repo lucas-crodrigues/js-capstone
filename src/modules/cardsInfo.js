a0d1dae202b7448c1742d2c309a7dd98892901bb
lucascr <lcrodrigues1995@gmail.com>
2022-07-27 14:05:07 -0300

    Update cardsInfo.js

--- src/modules/cardsInfo.js
+++ src/modules/cardsInfo.js
@@ -1,14 +1,15 @@
+import { getLocalStorage, setLocalStorage } from './localStorage.js';
 import getCardNames from './sessionCards.js';
 
 const getCardInfo = async (renderCards) => {
   const cardInfo = [];
   await getCardNames();
-  const sessionCards = JSON.parse(localStorage.getItem('sessionCards'));
+  const sessionCards = getLocalStorage('sessionCards');
   sessionCards.forEach(async (card) => {
     await fetch(`https://api.scryfall.com/cards/named?fuzzy=${card}`).then(async (response) => {
       const dataArr = await response.json();
       cardInfo.push(dataArr);
-      localStorage.setItem('sessionCardsInfo', JSON.stringify(cardInfo));
+      setLocalStorage('sessionCardsInfo', cardInfo);
     });
   });
   renderCards();
