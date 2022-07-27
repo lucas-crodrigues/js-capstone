6875a1f0a1c2cc9f720526ab6d51732da7569e5c
lucascr <lcrodrigues1995@gmail.com>
2022-07-27 13:36:52 -0300

    Update sessionCards.js

--- src/modules/sessionCards.js
+++ src/modules/sessionCards.js
@@ -1,13 +1,13 @@
 const url = 'https://api.scryfall.com/catalog/card-names';
 
 const getCardNames = async () => {
-  fetch(url).then(async (response) => {
+  const cardList = await fetch(url).then(async (response) => {
     const dataArr = await response.json();
     const resultArr = dataArr.data;
-    const filterResult = resultArr.splice(Math.floor(Math.random()*(resultArr.length - 24)), 24);
-    localStorage.setItem('sessionCards', JSON.stringify(filterResult));
+    const filterResult = resultArr.splice(Math.floor(Math.random() * (resultArr.length - 24)), 24);
     return filterResult;
   });
+  localStorage.setItem('sessionCards', JSON.stringify(cardList));
 };
 
 export default getCardNames;
