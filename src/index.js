6d653891e1ce2ae727909f43d5d1e3e8f5fa6815
lucascr <lcrodrigues1995@gmail.com>
2022-07-27 13:36:44 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import './styles.css';
 import getCardInfo from './modules/cardsInfo.js';
-import getCardNames from './modules/sessionCards.js';
 import renderCards from './modules/populateHTML.js';
 
 window.addEventListener('DOMContentLoaded', async () => {
-  getCardNames().then(getCardInfo());
-});
-
-document.querySelector('.logo').addEventListener('click', () => {
-  document.querySelector('.cards').innerHTML = '';
-  renderCards();
+  getCardInfo(renderCards);
 });
